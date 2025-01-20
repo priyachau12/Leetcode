e2563ed7005847f3d77278dbb9e920b8e4345762
Priya Chaurasiya <125198162+priyachau12@users.noreply.github.com>
2025-01-21 00:16:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (20.15%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,55 @@
+#include <queue>
+using namespace std;
+
+class MyStack {
+private:
+    queue<int> q1; // Main queue to hold elements
+    queue<int> q2; // Auxiliary queue for operations
+
+public:
+    MyStack() {
+        // Constructor
+    }
+    
+    // Push element x onto stack
+    void push(int x) {
+        q2.push(x); // Push the new element into the auxiliary queue
+        
+        // Transfer all elements from q1 to q2
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        
+        // Swap the names of q1 and q2
+        swap(q1, q2);
+    }
+    
+    // Removes the element on the top of the stack and returns it
+    int pop() {
+        if (q1.empty()) return -1; // Edge case: stack is empty
+        int topElement = q1.front();
+        q1.pop();
+        return topElement;
+    }
+    
+    // Get the top element
+    int top() {
+        if (q1.empty()) return -1; // Edge case: stack is empty
+        return q1.front();
+    }
+    
+    // Returns whether the stack is empty
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
